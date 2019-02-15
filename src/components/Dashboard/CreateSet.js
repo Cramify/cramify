cd6f6f8369d425cc35a12799f641af293a669ba2
Thai Bui <tbui64@gmail.com>
2019-02-15 16:56:01 -0700

    Styled Create Set Page

--- src/components/Dashboard/CreateSet.js
+++ src/components/Dashboard/CreateSet.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './CreateSet.scss';
+import {Link} from 'react-router-dom';
 
 
 export default class CreateSet extends Component {
@@ -27,18 +29,18 @@ export default class CreateSet extends Component {
         })
     }
 
-    addQuestionToSet = () => {
-        const {setID} = this.state
-        console.log(setID)
-        this.state.set.map((question) => {
-            return axios.post('/set/user/question', { setID, questionID: question.question_id })
-        })
-    }
+    // addQuestionToSet = () => {
+    //     const {setID} = this.state
+    //     console.log(setID)
+    //     this.state.set.map((question) => {
+    //         return axios.post('/set/user/question', { setID, questionID: question.question_id })
+    //     })
+    // }
 
     addToSet = (i) => {
         const newSet = this.state.set.slice();
         newSet.push(this.state.questions.splice(i, 1)[0]);
-        if(this.state.set.length >= 19){
+        if (this.state.set.length >= 19) {
             this.setState({
                 limitReached: true
             })
@@ -49,43 +51,75 @@ export default class CreateSet extends Component {
         })
     }
 
-    createNewSet = async() => {
-       const {setName} = this.state;
-       const res = await axios.post('/set/user/create', {setName})
-       this.setState({
-           setID: res.data[0].set_id,
-           setName: ''
-       })
-       console.log(this.state.setID)
+    createNewSet = async () => {
+        //Creates empty set to add to
+        const { setName } = this.state;
+        const res = await axios.post('/set/user/create', { setName })
+        this.setState({
+            setID: res.data[0].set_id,
+            setName: ''
+        })
+        //sends questions to add to new set
+        const { setID } = this.state
+        console.log(setID)
+        this.state.set.map((question) => {
+            return axios.post('/set/user/question', { setID, questionID: question.question_id })
+        })
+
+    }
+
+    deleteQuestionFromSet = (i) => {
+        console.log(i)
+        let backToQuestions = [];
+        let deleteFromSet = this.state.set.splice(i, 1);
+        backToQuestions.push(deleteFromSet);
+        console.log(backToQuestions[0][0])
+        this.setState({
+            questions: [...this.state.questions, backToQuestions[0][0]]
+        })
+
     }
 
     render() {
         let questions = this.state.questions.map((question, i) => {
             return (
-                <div key={i}>
-                    {question.category}
-                    <br />
-                    <button onClick={()=>this.addToSet(i)}>Add</button>
+                <div className='question-to-display' key={i} onClick={() => this.addToSet(i)} >
+                    <div className='question-category'>{question.category}</div>
+                     <br />
                     {question.question}
                 </div>
             )
         })
-        let set = this.state.set.map((question) => {
+        let set = this.state.set.map((question, i) => {
             return (
-                <div>
-                {question.question}
+                <div className='question-to-display' key={i} onClick={() => this.deleteQuestionFromSet(i)}>
+                     <div className='question-category'>{question.category}</div>
+                      <br/>
+                    {question.question} 
                 </div>
             )
         })
         return (
-            <div>
-                Create New Set
-                <input onChange={(e)=>this.handleInput('setName', e)} value={this.state.setName} type="text" placeholder="Your Set's Name"/>
-                <button onClick={this.createNewSet}>Create New Set</button>
-                {this.state.limitReached === false && <h2>Questions to Add: {questions}</h2>}
-                <hr/>
-                <h2>Your Questions: {set}</h2> {/*Change 'your questions' to the actual name of set created in Dashboard */}
-                <button onClick={this.addQuestionToSet}>Add New Set</button>
+            <div className='create-set-page'>
+                <Link to='/dashboard'><div></div></Link>
+                
+                <div className='question-list'>
+                    {this.state.limitReached === false && 
+                    <div>
+                        <h1 className='title'>Questions To Add:</h1> 
+                        <h1 >{questions}</h1>
+                    </div>}
+                </div>
+
+                <div className='question-list'>
+                    <h2 className='title'>Your Questions:</h2> {/*Change 'your questions' to the actual name of set created in Dashboard */}
+                    <div className='user-input' >
+                        <input onChange={(e) => this.handleInput('setName', e)} value={this.state.setName} type="text" placeholder="Your Set's Name" />
+                        <i class="fas fa-plus" onClick={this.createNewSet}></i>
+                      <div>{set}</div>
+                    </div>
+                </div>
+
             </div>
         )
     }
