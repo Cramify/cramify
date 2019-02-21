a0feb8932f518a845db362d766993717c187ec83
Jonathan McDonald <Jonathan@Jonathan.local>
2019-02-21 10:11:02 -0700

    timer moved to server for better synchronization

--- src/components/GameRoom/GameRoom.js
+++ src/components/GameRoom/GameRoom.js
@@ -31,6 +31,7 @@ class GameRoom extends Component {
     this.socket.on("run begin function", data => this.startGame());
     this.socket.on("display points", data => this.displayPoints(data));
     this.socket.on("set myid on state", data => this.updateMyID(data));
+    this.socket.on('next question', data => this.nextQuestion())
   }
 
   componentDidMount = async () => {
@@ -158,11 +159,12 @@ class GameRoom extends Component {
   };
 
   nextQuestion = () => {
+    if (this.state.currentQuestion + 2 > this.state.set.length) return this.socket.emit('end timer')
     this.setState({
       currentQuestion: this.state.currentQuestion + 1,
       questionDisplay: !this.state.questionDisplay
     });
-    console.log(this.state.set.length)
+    // console.log(this.state.set.length)
     if (this.state.currentQuestion + 2 > this.state.set.length) {
       //get rid of the timer
       return this.setState({ showTimer: false, questionDisplay: true });
