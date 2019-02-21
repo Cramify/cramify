5de61472b3a6d6fd861bb71f2687ea07ff1efc50
Allen Carlson <allen.carlson123@gmail.com>
2019-02-21 11:59:27 -0700

    added delete button on editUser

--- db/delete_user.sql
+++ db/delete_user.sql
@@ -1,2 +1,11 @@
+DELETE FROM question_junction
+where set_id = (
+  SELECT set_id FROM question_set
+  WHERE user_id = ${user_id}
+);
+
+DELETE FROM question_set
+where user_id = ${user_id};
+
 DELETE FROM user_account 
-WHERE user_id=${user_id};
+WHERE user_id = ${user_id};
